f24fd31cd58dea5f68bca6f79eb2ae8b6b09539a
probacska321 <probacska321@citromail.hu>
2020-01-20 23:17:13 +0100

    newest commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-pi-example
 this is an example for git demonstatration from youtube
+
+make a change
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def main(msg):
+    // another message
     // Adding a different message.
 	// print a message
     print(msg)
